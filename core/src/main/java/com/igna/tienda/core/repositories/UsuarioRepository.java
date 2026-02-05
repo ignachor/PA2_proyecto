316cdce18d9360aef8beb10bf793b3851d9e3089
ignachor <josecrucillo2024@gmail.com>
2026-02-05 17:25:46 -0300

    05022026_se cambio de lugar inMemoryRepository y se creo un archivo docker para levantar la BD local

--- core/src/main/java/com/igna/tienda/core/repositories/UsuarioRepository.java
+++ core/src/main/java/com/igna/tienda/core/repositories/UsuarioRepository.java
@@ -1,6 +1,5 @@
 package com.igna.tienda.core.repositories;
 import com.igna.tienda.core.domain.Usuario;
-import java.util.Optional;
 import java.util.UUID;
 public interface UsuarioRepository {
     Usuario buscarPorEmail(String email);
