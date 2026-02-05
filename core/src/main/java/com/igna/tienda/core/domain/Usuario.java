2250cac55a0be3c76e7016d6bd29e43d0ad12db5
ignachor <josecrucillo2024@gmail.com>
2026-02-05 14:15:03 -0300

    50202026_creacionClaseUsuario

--- core/src/main/java/com/igna/tienda/core/domain/Usuario.java
+++ core/src/main/java/com/igna/tienda/core/domain/Usuario.java
@@ -1,4 +1,54 @@
 package com.igna.tienda.core.domain;
+import com.igna.tienda.core.domain.enums.Rol;
+import java.util.UUID;
 
 public class Usuario {
+    private UUID id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private boolean activo; 
+    private String passwordHash;
+    private Rol rol;
+
+    public Usuario(UUID id, String nombre, String apellido, String email, String passwordHash, Rol rol) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.rol = rol;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean esCliente() {
+        return rol == Rol.CLIENTE;
+    }
+    
+    public boolean esAdmin() {
+        return rol == Rol.ADMIN;
+    }
+
+    public void desactivar() {
+        this.activo = false;
+    }
+
+    public void activar() {
+        this.activo = true;
+    }
 }
